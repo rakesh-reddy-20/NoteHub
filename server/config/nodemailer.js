ab31f0812a64630a5d1f96a32b28951bc8b4c361
Rakesh <150646901+rakesh-reddy-20@users.noreply.github.com>
2025-11-01 08:07:58 +0530

    edit dependies

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: process.env.SMTP_SECURE === "true", // true for 465, false for other ports
+  port: Number(process.env.SMTP_PORT),
+  secure: false, // always false for port 587
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
