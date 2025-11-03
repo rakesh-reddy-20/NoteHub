7a1dc424188688e4a786e8e90c8dcccc9d6174d8
Rakesh <150646901+rakesh-reddy-20@users.noreply.github.com>
2025-11-03 22:21:02 +0530

    CORS correction

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -38,8 +38,8 @@ const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // required on Render
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: none,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
--- server/server.js
+++ server/server.js
@@ -17,17 +17,22 @@ import connectDB from "./config/db.js";
 // Initialize Express app
 const app = express();
 const PORT = process.env.PORT || 8080;
-const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://notehub.vercel.app", // your frontend production URL
+];
 
 // Middleware
 app.use(express.json());
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin) return callback(null, true); // allow server-to-server or tools like Postman
+      if (!origin) return callback(null, true);
+      const hostname = new URL(origin).hostname;
+
       if (
         allowedOrigins.includes(origin) ||
-        /\.vercel\.app$/.test(new URL(origin).hostname) // allow all vercel subdomains
+        /\.vercel\.app$/.test(hostname) // allow all vercel subdomains
       ) {
         callback(null, true);
       } else {
